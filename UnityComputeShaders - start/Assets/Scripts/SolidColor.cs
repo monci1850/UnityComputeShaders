a2ba660973389bcf8a322835e37504c48df5b995
monci1850 <monci1850@gmail.com>
2023-12-07 20:14:53 +0800

    Test multiple kernels, draw circle, draw quad, step function, bitwise right shift operator

--- UnityComputeShaders - start/Assets/Scripts/SolidColor.cs
+++ UnityComputeShaders - start/Assets/Scripts/SolidColor.cs
@@ -6,6 +6,7 @@ public class SolidColor : MonoBehaviour
 
     public ComputeShader shader;
     public int texResolution = 256;
+    public string kernelName = "SolidRed";
 
     Renderer rend;
     RenderTexture outputTexture;
@@ -27,8 +28,9 @@ public class SolidColor : MonoBehaviour
 
     private void InitShader()
     {
-        kernelHandle = shader.FindKernel("CSMain");
+        kernelHandle = shader.FindKernel(kernelName);
 
+        shader.SetInt("texResolution", texResolution);
         shader.SetTexture(kernelHandle, "Result", outputTexture);
  
         rend.material.SetTexture("_MainTex", outputTexture);
